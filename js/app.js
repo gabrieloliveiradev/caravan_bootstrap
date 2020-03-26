d03eac112386c87caa21d0d8b6b19b04328a6985
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2020-03-26 10:37:52 -0300

    adicionar páginas ao site

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  }) 
+//   Função que ativa a tooltip na página de planos
+$('.carousel').carousel({
+    interval: 3000
+  })
+//   Alterando a valocidade de passada do carrossel
